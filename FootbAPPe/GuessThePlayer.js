549467c3b2b21424c94629db661a95f4caa2885a
Alexandre <alexandre.boucard@epitech.eu>
2023-10-07 15:18:43 +0200

    feat(api): add api server that just return a basic message, and an ngrok system to connect from another device, and a bash file to launch everything at the same time

--- FootbAPPe/GuessThePlayer.js
+++ FootbAPPe/GuessThePlayer.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import axios from 'axios';
+import {NGROK_URL} from "@env"
 
 export default function GuessThePlayer() {
-  
+  const [responseData, setResponseData] = useState(null);
+
+  const fetchData = () => {
+    console.log("request /", NGROK_URL);
+    axios
+      .get(`${NGROK_URL}/api/data`, {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        }})
+      .then((response) => {
+        setResponseData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>Guess The Player</Text>
       </View>
+      <TouchableOpacity style={styles.button} onPress={fetchData}>
+        <Text style={styles.buttonText}>Fetch Data</Text>
+      </TouchableOpacity>
+      {responseData && (
+        <View style={styles.responseContainer}>
+          <Text style={styles.responseText}>Response from Server:</Text>
+          <Text style={styles.responseData}>{JSON.stringify(responseData)}</Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -16,6 +44,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#008000',
+    alignItems: 'center',
   },
   titleContainer: {
     alignItems: 'center',
@@ -24,5 +53,30 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 50,
     color: 'white',
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  responseContainer: {
+    marginTop: 20,
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 5,
+  },
+  responseText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  responseData: {
+    marginTop: 10,
+    fontSize: 16,
   },
 });
---
+++ FootbAPPe/start-ngrok.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ngrok http 5002 > /dev/null &
+
+sleep 1
+
+NGROK_URL=$(curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+
+echo "NGROK_URL=$NGROK_URL"
+echo "NGROK_URL=$NGROK_URL" > .env
+
+expo start --tunnel
