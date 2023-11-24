39dad411be10f509691aebbde00c56df967958a9
Alexandre <alexandre.boucard@epitech.eu>
2023-11-24 13:21:49 +0100

    feat(levels_menu): first version with just a scrollable menu with buttons that leads nowhere

--- FootbAPPe/CarrierTracer.js
+++ FootbAPPe/CarrierTracer.js
@@ -1,13 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
 
 export default function CarrierTracer() {
+  const stringsArray = [
+    'Zlatan Ibrahimovic',
+    'Antoine Griezmann',
+    'Hugo Lloris',
+    'Angel Di Maria',
+    'Thiago Alcantara',
+    'Alvaro Morata',
+    'Xavi Simons',
+    'Raphaël Leao',
+    'Ludovic Blas',
+    ' Alex Grimaldo',
+  ];
+
+  const handlePress = (text) => {
+    console.log(text); // Display text in the console
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>Carrier Tracer</Text>
       </View>
+
+      {/* Mapping through the stringsArray */}
+      <ScrollView>
+        {stringsArray.map((text, index) => (
+          <TouchableOpacity
+            key={index}
+            style={styles.touchableOpacity}
+            onPress={() => handlePress(text)}
+          >
+            <Text>{text}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -24,5 +53,14 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 50,
     color: 'white',
+  },
+  touchableOpacity: {
+    backgroundColor: '#B3EFB2',
+    padding: 10,
+    margin: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    height: 100,
   },
 });
